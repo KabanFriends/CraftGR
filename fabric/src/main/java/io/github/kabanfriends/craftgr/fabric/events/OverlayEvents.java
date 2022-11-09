de26c7d027036f29275a4ed204bb267c32c9022f
KabanFriends <yuukun2004@protonmail.ch>
2022-11-09 21:35:03 +0900

    Use more fabric events

--- fabric/src/main/java/io/github/kabanfriends/craftgr/fabric/events/OverlayEvents.java
+++ fabric/src/main/java/io/github/kabanfriends/craftgr/fabric/events/OverlayEvents.java
@@ -5,19 +5,21 @@ import io.github.kabanfriends.craftgr.handler.OverlayHandler;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
+import net.fabricmc.fabric.api.client.screen.v1.ScreenMouseEvents;
 
-public class HudRenderEvents implements ClientModInitializer {
+public class OverlayEvents implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        //Rendering in HUD
         HudRenderCallback.EVENT.register((poseStack, delta) -> {
             if (CraftGR.MC.screen == null) OverlayHandler.renderAll(poseStack, 0, 0);
         });
 
+        //Screen events
         ScreenEvents.BEFORE_INIT.register(((client, screen, scaledWidth, scaledHeight) -> {
-            ScreenEvents.afterRender(screen).register(((renderScreen, poseStack, mouseX, mouseY, tickDelta) -> {
-                OverlayHandler.renderAll(poseStack, mouseX, mouseY);
-            }));
+            ScreenEvents.afterRender(screen).register(((eventScreen, poseStack, mouseX, mouseY, tickDelta) -> OverlayHandler.renderAll(poseStack, mouseX, mouseY)));
+            ScreenMouseEvents.allowMouseClick(screen).register(((eventScreen, mouseX, mouseY, button) -> !OverlayHandler.clickPressAll((int)mouseX, (int)mouseY)));
         }));
     }
 
